41e5ab700a47d1adb4a54a533e7da525ac753f73
Mhayk Whandson <hi@mhayk.com>
2023-09-12 17:49:44 +0100

    feat: create and style textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Github } from 'lucide-react'
 import { Button } from "./components/ui/button";
 import { Separator } from './components/ui/separator';
+import { Textarea } from './components/ui/textarea';
 
 export function App() {
   return (
-    <div>
+    <div className='min-h-screen flex flex-col'>
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">upload.ai</h1>
 
@@ -19,6 +20,25 @@ export function App() {
           </Button>
         </div>
       </div>
+
+      <main className='flex-1 p-6 flex gap-6'>
+        <div className='flex flex-col flex-1 gap-4'>
+          <div className='grid grid-rows-2 gap-4 flex-1'>
+            <Textarea
+              className='resize-none p-4 leading-relaxed'
+              placeholder='Inclua o prompt para a IA...'
+            />
+            <Textarea
+              className='resize-none p-4 leading-relaxed'
+              placeholder='Resultado gerado pela IA...'
+            />
+          </div>
+
+          <p className='text-sm text-muted-foreground'>
+            Lembre-se: você pode utilizar a variável <code className='text-violet-400'>{'{transcription}'}</code> no seu prompt para adionar o conteúdo da transcrição do vídeo selecionado.</p>
+        </div>
+        <aside className='w-80'></aside>
+      </main>
     </div>
   )
 }
